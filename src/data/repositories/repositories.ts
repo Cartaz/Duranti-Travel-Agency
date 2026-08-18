be4cbb62761a25d14a6902909dbfb22c74584cf6
Cartaz <prenc.francesco@gmail.com>
2026-08-18 23:43:11 +0200

    feat: expose typed repositories

---
+++ src/data/repositories/repositories.ts
@@ -0,0 +1,18 @@
+import { db } from '../db/duranti-db'
+import { Repository } from './base-repository'
+import type { DatabaseTables } from '../db/schema'
+
+export const tripRepository = new Repository<DatabaseTables['trips']>(db.trips)
+export const travelerRepository = new Repository<DatabaseTables['travelers']>(db.travelers)
+export const travelerDocumentRepository = new Repository<DatabaseTables['travelerDocuments']>(db.travelerDocuments)
+export const tripTravelerRepository = new Repository<DatabaseTables['tripTravelers']>(db.tripTravelers)
+export const dayRepository = new Repository<DatabaseTables['days']>(db.days)
+export const blockRepository = new Repository<DatabaseTables['blocks']>(db.blocks)
+export const placeRepository = new Repository<DatabaseTables['places']>(db.places)
+export const mediaRepository = new Repository<DatabaseTables['media']>(db.media)
+export const linkRepository = new Repository<DatabaseTables['links']>(db.links)
+export const itineraryRepository = new Repository<DatabaseTables['itineraries']>(db.itineraries)
+export const templateRepository = new Repository<DatabaseTables['templates']>(db.templates)
+export const expenseRepository = new Repository<DatabaseTables['expenses']>(db.expenses)
+export const reservationRepository = new Repository<DatabaseTables['reservations']>(db.reservations)
+export const auditRepository = new Repository<DatabaseTables['auditLog']>(db.auditLog)
