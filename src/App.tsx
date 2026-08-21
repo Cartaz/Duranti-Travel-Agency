d4c5307c143c24eb7e8ecde3505a5f69a4443ab1
Cartaz <prenc.francesco@gmail.com>
2026-08-21 07:56:58 +0200

    feat: route day planner through guided add flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import DayFormPage from './features/days/DayFormPage'
-import DayPlannerPage from './features/planner/DayPlannerPage'
+import GuidedDayPlannerPage from './features/planner/GuidedDayPlannerPage'
 import StorageLabPage from './features/storage-lab/StorageLabPage'
 import ArchivedTripsPage from './features/trips/ArchivedTripsPage'
 import TravelIndexPage from './features/trips/TravelIndexPage'
@@ -24,7 +24,7 @@ export default function App() {
           <Route path="trips/:tripId" element={<TripDetailPage />} />
           <Route path="trips/:tripId/edit" element={<TripFormPage mode="edit" />} />
           <Route path="trips/:tripId/days/new" element={<DayFormPage mode="create" />} />
-          <Route path="trips/:tripId/days/:dayId" element={<DayPlannerPage />} />
+          <Route path="trips/:tripId/days/:dayId" element={<GuidedDayPlannerPage />} />
           <Route path="trips/:tripId/days/:dayId/edit" element={<DayFormPage mode="edit" />} />
           <Route path="lab" element={<StorageLabPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
