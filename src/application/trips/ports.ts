415ae35228db29deefe50ee25e4e5ecb02e8b173
Cartaz <prenc.francesco@gmail.com>
2026-08-25 10:25:13 +0200

    arch: define trip application ports

---
+++ src/application/trips/ports.ts
@@ -0,0 +1,18 @@
+import type { Day, Trip } from '../../domain/entities'
+
+export interface TripRepositoryPort {
+  list(): Promise<Trip[]>
+  get(id: string): Promise<Trip | undefined>
+  put(value: Trip): Promise<void>
+}
+
+export interface DayRepositoryPort {
+  list(): Promise<Day[]>
+}
+
+export interface TripApplicationDependencies {
+  trips: TripRepositoryPort
+  days: DayRepositoryPort
+  now(): string
+  newId(): string
+}
