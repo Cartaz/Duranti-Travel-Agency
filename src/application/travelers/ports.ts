ed0e46d3287d1288fef76788cd9ec2a687f76198
Cartaz <prenc.francesco@gmail.com>
2026-08-25 13:00:39 +0200

    arch: add traveler application ports

---
+++ src/application/travelers/ports.ts
@@ -0,0 +1,26 @@
+import type { Traveler, Trip, TripTraveler } from '../../domain/entities'
+
+export type TravelerRole = 'owner' | 'companion' | 'child' | 'other'
+
+export interface TravelerRepositoryPort {
+  list(): Promise<Traveler[]>
+  get(id: string): Promise<Traveler | undefined>
+  put(value: Traveler): Promise<unknown>
+}
+
+export interface TravelerTripPort { get(id: string): Promise<Trip | undefined> }
+
+export interface TripTravelerPort {
+  listActiveForTrip(tripId: string): Promise<TripTraveler[]>
+  setMembership(tripId: string, travelerId: string, role: TravelerRole): Promise<TripTraveler>
+  detachMembership(tripId: string, travelerId: string): Promise<unknown>
+}
+
+export interface TravelerApplicationDependencies {
+  travelers: TravelerRepositoryPort
+  trips: TravelerTripPort
+  memberships: TripTravelerPort
+  now(): string
+  newId(): string
+  today(): string
+}
