2a90891a16fd912430ff716c8b81ee371d669048
Cartaz <prenc.francesco@gmail.com>
2026-08-25 19:14:21 +0200

    test: run historical media place contract

--- tests/browser/application-harness.ts
+++ tests/browser/application-harness.ts
@@ -1,9 +1,11 @@
 import { runApplicationContractTests } from './application-contracts'
 import { runItineraryQueryContract } from './itinerary-query-contract'
+import { runMediaHistoricalPlaceContract } from './media-historical-place-contract'
 
 const results = [
   ...(await runApplicationContractTests()),
   await runItineraryQueryContract(),
+  await runMediaHistoricalPlaceContract(),
 ]
 ;(window as Window & { __DTAGENCY_APPLICATION_RESULTS__?: unknown }).__DTAGENCY_APPLICATION_RESULTS__ = results
 
