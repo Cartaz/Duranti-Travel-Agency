6eff7ff409c0c5ccee6fd43c96fc3c0659fa5268
Cartaz <prenc.francesco@gmail.com>
2026-08-26 20:31:48 +0200

    feat: route travel book reader

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import DayFormPage from './features/days/DayFormPage'
 import GuidedDayPlannerPage from './features/planner/GuidedDayPlannerPage'
 import StorageLabPage from './features/storage-lab/StorageLabPage'
+import TravelBookPage from './features/travel-book/TravelBookPage'
 import ArchivedTripsPage from './features/trips/ArchivedTripsPage'
 import TravelIndexPage from './features/trips/TravelIndexPage'
 import TripDetailPage from './features/trips/TripDetailPage'
@@ -27,6 +28,7 @@ export default function App() {
           <Route path="trips/new" element={<TripFormPage mode="create" />} />
           <Route path="trips/:tripId" element={<TripDetailPage />} />
           <Route path="trips/:tripId/edit" element={<TripFormPage mode="edit" />} />
+          <Route path="trips/:tripId/book" element={<TravelBookPage />} />
           <Route path="trips/:tripId/days/new" element={<DayFormPage mode="create" />} />
           <Route path="trips/:tripId/days/:dayId" element={<GuidedDayPlannerPage />} />
           <Route path="trips/:tripId/days/:dayId/edit" element={<DayFormPage mode="edit" />} />
