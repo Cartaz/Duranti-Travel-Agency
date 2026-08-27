3b42f9dbd82fba7744d0b21bb9852cfd8f955ac6
Cartaz <prenc.francesco@gmail.com>
2026-08-27 07:37:23 +0200

    feat: add media reader port to travel book

--- src/application/travel-book/ports.ts
+++ src/application/travel-book/ports.ts
@@ -1,4 +1,4 @@
-import type { Day, Trip } from '../../domain/entities'
+import type { Day, Media, Trip } from '../../domain/entities'
 
 export interface TravelBookTripReader {
   getTrip(tripId: string): Promise<Trip | undefined>
@@ -8,7 +8,13 @@ export interface TravelBookDayReader {
   listTripDays(tripId: string): Promise<Day[]>
 }
 
+export interface TravelBookMediaReader {
+  listDayMedia(tripId: string, dayId: string): Promise<Media[]>
+  readMediaFile(mediaId: string): Promise<File>
+}
+
 export interface TravelBookApplicationDependencies {
   trips: TravelBookTripReader
   days: TravelBookDayReader
+  media: TravelBookMediaReader
 }
