c8d2eb108cf656b4533961c174a6097428638626
Cartaz <prenc.francesco@gmail.com>
2026-08-27 10:58:26 +0200

    wire atomic day template transaction

--- src/composition/templates.ts
+++ src/composition/templates.ts
@@ -1,13 +1,13 @@
 import { createDayTemplateApplication } from '../application/templates/day-template-application'
+import { dayTemplateTransactionRepository } from '../data/repositories/day-template-transaction-repository'
 import { blockRepository, dayRepository, templateRepository, tripRepository } from '../data/repositories/repositories'
-import { dayApplication } from './days'
 
 export const dayTemplateApplication = createDayTemplateApplication({
   templates: templateRepository,
   blocks: blockRepository,
   days: dayRepository,
   trips: tripRepository,
-  dayCreator: dayApplication,
+  transactions: dayTemplateTransactionRepository,
   now: () => new Date().toISOString(),
   newId: () => crypto.randomUUID(),
 })
